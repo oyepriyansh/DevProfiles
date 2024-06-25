3f2e27108ed0c9046e76392e4ae79804cb891052
Priyansh Prajapat <oyepriyansh@duck.com>
2024-06-25 08:39:24 +0530

    404 page code refactor

--- scripts/404.js
+++ scripts/404.js
@@ -1,5 +1,5 @@
-
 'use strict';
+
 var e, t;
 new function(e) {
     const t = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-=+<>,./?[{()}]!@#$%^&*~`|".split(""),
@@ -22,3 +22,51 @@ new function(e) {
         n.style.display = "block", o.style.display = "none", o.innerText = "", s = 0, void 0 !== r && (window.clearInterval(r), r = void 0), void 0 !== l && (window.clearInterval(l), l = void 0), void 0 !== i && (window.clearInterval(i), i = void 0)
     }
 }(document.getElementById("error_text")).start(), "en" !== navigator.language.substring(0, 2).toLowerCase() && (e = document.createElement("script"), t = document.body, e.src = "", e.async = e.defer = !0, e.addEventListener("load", (() => t.removeChild(e))), t.appendChild(e));
+
+
+var style = document.createElement('style');
+style.type = 'text/css';
+style.innerHTML = `
+    @import url("https://fonts.googleapis.com/css2?family=PT+Serif&family=Poppins:wght@200&display=swap");
+
+    body,
+    html {
+        margin: 0;
+        background-color: #222;
+        color: #aaa;
+        font-family: "Poppins", sans-serif;
+        font-size: 0
+    }
+
+    .full-height {
+        height: 100vh
+    }
+
+    .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center
+    }
+
+    #error_text {
+        font-size: 32px
+    }
+
+    button {
+        display: inline-block;
+        padding: 10px 20px;
+        background-color: #2b3031;
+        border: none;
+        color: #fff;
+        border-radius: 4px;
+        margin-top: 10px;
+        font-size: 16px;
+        cursor: pointer;
+        transition: all .3s ease-in-out
+    }
+
+    button:hover {
+        background-color: #555
+    }
+`;
+document.head.appendChild(style);

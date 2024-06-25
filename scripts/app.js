b9d8bde853648168c372adbdf09a4a46047fc213
Priyansh Prajapat <oyepriyansh@duck.com>
2024-06-25 07:30:22 +0530

    ui: remove theme toggle, and better footer

--- scripts/app.js
+++ scripts/app.js
@@ -57,48 +57,6 @@ const shuffleArray = (array) => {
 loadProfiles();
 
 
-// dark light mode 
-
-const colorSwitch = document.getElementById("input-toggle-button");
-colorSwitch.addEventListener("click", checkMode);
-
-const btn = document.querySelector('.add-col');
-
-function checkMode() {
-  console.log("checking...");
-  if (colorSwitch.checked) {
-    console.log("dark on");
-    darkModeOn();
-    btn.style.color = "black";
-    btn.addEventListener('mouseover', () => {
-      btn.style.color = "white";
-    })
-    btn.addEventListener('mouseout', () => {
-      btn.style.color = "black";
-    })
-  } else {
-    console.log("dark off");
-    darkModeOff();
-    btn.style.color = "white";
-    btn.addEventListener('mouseout', () => {
-      btn.style.color = "white";
-    })
-
-    btn.addEventListener('mouseover', () => {
-      btn.style.color = "white";
-    })
-  }
-}
-
-function darkModeOn() {
-  document.body.classList.add("dark-mode");
-}
-
-function darkModeOff() {
-  document.body.classList.remove("dark-mode");
-}
-
-
 //search 
 
 searchInput.addEventListener('keyup', () => {

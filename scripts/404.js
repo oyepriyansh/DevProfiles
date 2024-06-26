94980572de712d762baec468ab0988c74a97b0ba
Priyansh Prajapat <oyepriyansh@duck.com>
2024-06-26 13:32:43 +0530

    refactor code

--- scripts/404.js
+++ scripts/404.js
@@ -7,10 +7,12 @@ new function(e) {
         o = e.querySelector(".target");
     let r, i, l, s = 0;
     this.start = function() {
-        n.style.display = "none", o.style.display = "block", r = window.setInterval((() => {
+        n.style.display = "none", o.style.display = "block",
+         r = window.setInterval((() => {
             s <= n.innerText.length && (o.innerText = n.innerText.substring(0, s) + function(e) {
                 let n = "";
-                for (let o = 0; o < e; o++) n += t[Math.floor(Math.random() * t.length)];
+                for (let o = 0; o < e; o++) n += 
+                t[Math.floor(Math.random() * t.length)];
                 return n
             }(n.innerText.length - s))
         }), 15), i = window.setTimeout((() => {
@@ -19,10 +21,12 @@ new function(e) {
             }), 70)
         }), 350)
     }, this.stop = function() {
-        n.style.display = "block", o.style.display = "none", o.innerText = "", s = 0, void 0 !== r && (window.clearInterval(r), r = void 0), void 0 !== l && (window.clearInterval(l), l = void 0), void 0 !== i && (window.clearInterval(i), i = void 0)
+        n.style.display = "block", o.style.display = "none",
+        o.innerText = "", s = 0, void 0 !== r && (window.clearInterval(r), r = void 0),
+        void 0 !== l && (window.clearInterval(l), l = void 0),
+        void 0 !== i && (window.clearInterval(i), i = void 0)
     }
 }(document.getElementById("error_text")).start(), "en" !== navigator.language.substring(0, 2).toLowerCase() && (e = document.createElement("script"), t = document.body, e.src = "", e.async = e.defer = !0, e.addEventListener("load", (() => t.removeChild(e))), t.appendChild(e));
-
 
 var style = document.createElement('style');
 style.type = 'text/css';
--- scripts/app.js
+++ scripts/app.js
@@ -2,37 +2,33 @@ const container = document.querySelector('.container');
 const defaultImage = "https://oyepriyansh.pages.dev/i/5nf5fd.png";
 
 const loadProfiles = async () => {
+  // Fetching data from JSON file
   let data = await fetch('/data.json');
   let profiles = await data.json();
 
   profiles = shuffleArray(profiles);
 
+  
   profiles.forEach((profile) => {
     let profileDiv = document.createElement('div');
     profileDiv.classList.add('profile');
 
+    //skills 
     let skills = profile.skills.map(skill => `<span class="skill">${skill}</span>`).join('');
 
+    // social links
     let social = '';
-
     if (profile.github) {
-      social += `
-        <a href="${profile.github}" target="_blank"><i class="fa-brands fa-github"></i></a>
-      `;
+      social += `<a href="${profile.github}" target="_blank"><i class="fa-brands fa-github"></i></a>`;
     }
-
     if (profile.twitter) {
-      social += `
-        <a href="${profile.twitter}" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
-      `;
+      social += `<a href="${profile.twitter}" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>`;
     }
-
     if (profile.linkedin) {
-      social += `
-        <a href="${profile.linkedin}" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
-      `;
+      social += `<a href="${profile.linkedin}" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>`;
     }
 
+    // adding profile HTML content
     profileDiv.innerHTML = `
       <div class="pfp">
         <img src="${profile.image}" alt="User Image" onerror="this.onerror=null; this.src='${defaultImage}';">
@@ -46,6 +42,7 @@ const loadProfiles = async () => {
   });
 };
 
+// Function to shuffle 
 const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -56,51 +53,49 @@ const shuffleArray = (array) => {
 
 loadProfiles();
 
-
-//search 
-
+// Search function
 searchInput.addEventListener('keyup', () => {
   const searchTerm = searchInput.value.trim().toLowerCase();
-
   const profiles = document.querySelectorAll('.profile');
-
   let visibleProfiles = 0;
+
+
 
   profiles.forEach((profile) => {
     const profileName = profile.querySelector('.name').innerText.trim().toLowerCase();
     const skills = profile.querySelector('.skills').textContent.toLowerCase();
 
     if (profileName.includes(searchTerm) || skills.includes(searchTerm)) {
-    profile.style.display = 'flex'; 
+      profile.style.display = 'flex';
       visibleProfiles++;
     } else {
-      profile.style.display = 'none'; 
+      profile.style.display = 'none';
     }
   });
 
+// no profiles
   const noProfileMessage = document.querySelector('.no-profile');
   if (visibleProfiles > 0) {
-    noProfileMessage.style.display = 'none'; 
+    noProfileMessage.style.display = 'none';
   } else {
-    noProfileMessage.style.display = 'block'; 
+    noProfileMessage.style.display = 'block';
   }
 });
 
-// scroll top button 
-
+// Scroll to top button        
 var fabButton = document.getElementById("backToTopBtn");
 
 window.onscroll = function () {
-    if (window.scrollY > 20) {
-        fabButton.style.display = "block";
-    } else {
-        fabButton.style.display = "none";
-    }
+  if (window.scrollY > 20) {
+    fabButton.style.display = "block";
+  } else {
+    fabButton.style.display = "none";
+  }
 };
 
 fabButton.addEventListener("click", function () {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
-// copyright year 
+// footer year
 document.getElementById("currentYear").textContent = new Date().getFullYear();
